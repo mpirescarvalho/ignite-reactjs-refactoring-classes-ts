0b758d1a2e38dbf525f1699e9d3531aa698ae216
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2021-07-16 00:43:11 -0300

    refactor javascript to typescript

---
+++ src/components/Food/index.tsx
@@ -0,0 +1,83 @@
+import { useState } from 'react';
+import { FiEdit3, FiTrash } from 'react-icons/fi';
+
+import { Container } from './styles';
+import api from '../../services/api';
+
+import type { FoodType } from '../../pages/Dashboard';
+
+interface FoodProps {
+  food: FoodType;
+  handleEditFood: (food: FoodType) => void
+  handleDelete: (foodId: number) => void
+}
+
+const Food = (props: FoodProps) => {
+  const [isAvailable, setIsAvailable] = useState(props.food.available);
+
+  async function toggleAvailable() {
+    await api.put(`/foods/${props.food.id}`, {
+      ...props.food,
+      available: !isAvailable,
+    });
+
+    setIsAvailable(!isAvailable)
+  }
+
+  function setEditingFood() {
+    props.handleEditFood(props.food);
+  }
+
+  return (
+    <Container available={isAvailable}>
+      <header>
+        <img src={props.food.image} alt={props.food.name} />
+      </header>
+      <section className="body">
+        <h2>{props.food.name}</h2>
+        <p>{props.food.description}</p>
+        <p className="price">
+          R$ <b>{props.food.price}</b>
+        </p>
+      </section>
+      <section className="footer">
+        <div className="icon-container">
+          <button
+            type="button"
+            className="icon"
+            onClick={setEditingFood}
+            data-testid={`edit-food-${props.food.id}`}
+          >
+            <FiEdit3 size={20} />
+          </button>
+
+          <button
+            type="button"
+            className="icon"
+            onClick={() => props.handleDelete(props.food.id)}
+            data-testid={`remove-food-${props.food.id}`}
+          >
+            <FiTrash size={20} />
+          </button>
+        </div>
+
+        <div className="availability-container">
+          <p>{isAvailable ? 'Disponível' : 'Indisponível'}</p>
+
+          <label htmlFor={`available-switch-${props.food.id}`} className="switch">
+            <input
+              id={`available-switch-${props.food.id}`}
+              type="checkbox"
+              checked={isAvailable}
+              onChange={toggleAvailable}
+              data-testid={`change-status-food-${props.food.id}`}
+            />
+            <span className="slider" />
+          </label>
+        </div>
+      </section>
+    </Container>
+  );
+};
+
+export default Food;
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,8 +9,13 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+type InputProps = {
+  name: string
+  icon?: (props: any) => JSX.Element
+} & JSX.IntrinsicElements['input']
+
+const Input = ({ name, icon: Icon, ...rest }: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -25,7 +30,7 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     setIsFocused(false);
 
     setIsFilled(!!inputRef.current?.value);
-  }, []);
+  }, []); 
 
   useEffect(() => {
     registerField({
